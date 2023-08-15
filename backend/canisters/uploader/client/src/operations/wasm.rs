4933d98e88b34eb3bcd74b4fc72d17aba367e271
slaigv <slaigv@gmail.com>
2023-08-15 11:43:54 +0300

    Publish uploader client 0.2.2

--- backend/canisters/uploader/client/src/operations/wasm.rs
+++ backend/canisters/uploader/client/src/operations/wasm.rs
@@ -1,23 +1,12 @@
 use std::fs::File;
 use std::io::Read;
-use std::path::PathBuf;
 
-pub fn get_canister_wasm(canister_name: String) -> Vec<u8> {
-    let file_name = canister_name + "_canister_impl-opt.wasm";
-    read_file_from_local_bin(&file_name)
-}
-
-pub fn read_file_from_local_bin(file_name: &str) -> Vec<u8> {
-    let mut file_path =
-        PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").expect("Failed to read CARGO_MANIFEST_DIR env variable"));
-
-    file_path.push("local-bin");
-    file_path.push(file_name);
-
-    let mut file = File::open(&file_path).unwrap_or_else(|_| panic!("Failed to open file: {}", file_path.to_str().unwrap()));
+fn get_canister_wasm(wasm_path: &str) -> Result<Vec<u8>, String> {
+    let mut file = File::open(wasm_path).map_err(|error| format!("Failed to open wasm file {wasm_path}: {error:?}"))?;
 
     let mut bytes = Vec::new();
-    file.read_to_end(&mut bytes).expect("Failed to read file");
+    file.read_to_end(&mut bytes)
+        .map_err(|error| format!("Failed to read wasm file {wasm_path}: {error:?}"))?;
 
-    bytes
+    Ok(bytes)
 }
