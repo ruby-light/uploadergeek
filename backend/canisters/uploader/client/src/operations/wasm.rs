1f4e00da7214f504601b9b95e7b7c69158882f08
slaigv <slaigv@gmail.com>
2023-08-15 11:49:07 +0300

    Publish uploader client 0.2.4: new get_wasm operation

--- backend/canisters/uploader/client/src/operations/wasm.rs
+++ backend/canisters/uploader/client/src/operations/wasm.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::Read;
 
-pub(crate) fn get_canister_wasm(wasm_path: &str) -> Result<Vec<u8>, String> {
+pub fn get_canister_wasm(wasm_path: &str) -> Result<Vec<u8>, String> {
     let mut file = File::open(wasm_path).map_err(|error| format!("Failed to open wasm file {wasm_path}: {error:?}"))?;
 
     let mut bytes = Vec::new();
