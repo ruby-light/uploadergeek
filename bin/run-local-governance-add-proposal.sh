1b1d08f2a7b79f5f03758632366e5b9efe8ef881
alexeyc <alexey.chirkov@rubylight.com>
2023-08-16 15:36:55 +0300

    script to give governance configuration permissions to local InternetIdentity user logged in on UI

--- bin/run-local-governance-add-proposal.sh
+++ bin/run-local-governance-add-proposal.sh
@@ -6,23 +6,31 @@ set -euo pipefail
 NETWORK_ALIAS=local
 IDENTITY=default
 
-header "Create governance proposal to add UI 10000 user in network '${NETWORK_ALIAS}' using identity '${IDENTITY}'"
+PRINCIPAL="ziqcz-ouk4n-ez2ek-bmwra-k2w7l-iimo6-2scbu-6nbip-hk6na-fnh43-zqe"
+
+header "Create governance proposal to add '${PRINCIPAL}' in network '${NETWORK_ALIAS}' using identity '${IDENTITY}'"
+
+echo "Proceed? (y/n)"
+read answer
+if [ "$answer" != "y" ]; then
+    echo "Cancelled..."
+    exit 1
+fi
 
 IDENTITY_PRINCIPAL="$(dfx --identity "$IDENTITY" identity get-principal)"
 
 PERMISSIONS_ALL="vec{record{variant{UpdateGovernance};vec{variant{Add};variant{Vote};variant{Perform}}};record{variant{CallCanister};vec{variant{Add};variant{Vote};variant{Perform}}};record{variant{UpgradeCanister};vec{variant{Add};variant{Vote};variant{Perform}}};}"
-LOCAL_II_10000_PRINCIPAL="eiwkf-df3ud-wh6dv-5gvkk-eotk6-4rjla-fb2ql-zpci4-2j6pw-tpju7-2qe"
 
-PARTICIPANT_LOCAL_II_10000="record{principal \"${LOCAL_II_10000_PRINCIPAL}\"; record{name=\"local_II_10000\";proposal_permissions=${PERMISSIONS_ALL}}; }"
-PARTICIPANTS="vec{${PARTICIPANT_LOCAL_II_10000}}"
+PARTICIPANT="record{principal \"${PRINCIPAL}\"; record{name=\"participant\";proposal_permissions=${PERMISSIONS_ALL}}; }"
+PARTICIPANTS="vec{${PARTICIPANT}}"
 
-VOTING_CONFIGURATION="vec {record{variant{UpdateGovernance};record{stop_vote_count=1;positive_vote_count=1}}}"
+VOTING_CONFIGURATION="vec {record{variant{UpdateGovernance};record{stop_vote_count=1;positive_vote_count=1}};record{variant{CallCanister};record{stop_vote_count=1;positive_vote_count=1}};record{variant{UpgradeCanister};record{stop_vote_count=1;positive_vote_count=1}}}"
 
 NEW_GOVERNANCE="record{participants=${PARTICIPANTS}; voting_configuration=${VOTING_CONFIGURATION}}"
 
 #1. Create proposal
-dfx canister call governance add_new_proposal "(record{proposal_detail=variant {UpdateGovernance = record{new_governance=${NEW_GOVERNANCE}}}})"
+#dfx canister --identity "$IDENTITY" --network "${NETWORK_ALIAS}" call governance add_new_proposal "(record{proposal_detail=variant {UpdateGovernance = record{new_governance=${NEW_GOVERNANCE}}}})"
 #2. Vote proposal
-dfx canister call governance vote_for_proposal "(record{vote=true;proposal_id=1})"
+#dfx canister --identity "$IDENTITY" --network "${NETWORK_ALIAS}" call governance vote_for_proposal "(record{vote=true;proposal_id=1})"
 #3. Perform proposal
-dfx canister call governance perform_proposal "(record{proposal_id=1})"
+#dfx canister --identity "$IDENTITY" --network "${NETWORK_ALIAS}" call governance perform_proposal "(record{proposal_id=1})"
